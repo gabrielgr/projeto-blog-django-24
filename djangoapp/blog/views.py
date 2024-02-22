f7403b3f73a0f4dae4e65249578b4dd6c0508612
Gabriel Remondini <ggr.gabriel@gmail.com>
2024-02-22 20:14:17 -0300

    Usando self.kwargs para argumentos de URL e ordem de chamada dos métodos em CBV

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -5,25 +5,19 @@ from django.contrib.auth.models import User
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.db.models.query import QuerySet
-from django.http import Http404
-from django.shortcuts import render
+from django.http import Http404, HttpRequest
+from django.http.response import HttpResponse as HttpResponse
+from django.shortcuts import redirect, render
 from django.views.generic import ListView
 
 PER_PAGE = 9
 
 
 class PostListView(ListView):
-    model = Post
     template_name = 'blog/pages/index.html'
     context_object_name = 'posts'
-    ordering = '-pk',
     paginate_by = PER_PAGE
     queryset = Post.objects.get_published()
-
-    # def get_queryset(self):
-    #     queryset = super().get_queryset()
-    #     queryset = queryset.filter(is_published=True)
-    #     return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -35,29 +29,43 @@ class PostListView(ListView):
         return context
 
 
-# def index(request):
-#     # Function Based Views -> São funções
-#     # Class Based Views -> São classes (POO)
+class CreatedByListView(PostListView):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._temp_context = {}
 
-#     # Obter dados do model
-#     # Esses dados são uma lista de objetos
-#     # Paginação
-#     # Renderização
-#     # Manipulando o contexto
-#     posts = Post.objects.get_published()
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        user = self._temp_context['user']
+        user_full_name = user.username
 
-#     paginator = Paginator(posts, PER_PAGE)
-#     page_number = request.GET.get("page")
-#     page_obj = paginator.get_page(page_number)
+        if user.first_name:
+            user_full_name = f'{user.first_name} {user.last_name}'
 
-#     return render(
-#         request,
-#         'blog/pages/index.html',
-#         {
-#             'page_obj': page_obj,
-#             'page_title': 'Home - ',
-#         }
-#     )
+        page_title = 'Posts de ' + user_full_name + ' - '
+        ctx.update({
+            'page_title': page_title
+        })
+
+        return ctx
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = qs.filter(created_by__pk=self._temp_context['user'].pk)
+        return qs
+
+    def get(self, request, *args, **kwargs):
+        author_pk = self.kwargs.get('author_pk')
+        user = User.objects.filter(pk=author_pk).first()
+
+        if user is None:
+            raise Http404()
+        self._temp_context.update({
+            'author_pk': author_pk,
+            'user': user,
+        })
+
+        return super().get(request, *args, **kwargs)
 
 
 def created_by(request, author_pk):
